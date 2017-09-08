7c0d24377f4fcc0ee0bedcccdbea59953ef58fbb
gcalabi <31721887+gcalabi@users.noreply.github.com>
2017-09-08 00:54:46 -0300

    Add files via upload

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -15,7 +15,7 @@ import org.springframework.context.ApplicationContext;
 public class Application {
 
     public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+        //ApplicationContext ctx = SpringApplication.run(Application.class, args);
 
         System.out.println("solo socket");
         escuchar();
